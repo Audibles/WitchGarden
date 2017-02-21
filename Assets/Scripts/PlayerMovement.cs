10f379b75d63927df0b4cc27472d85377124ce94
Audrey Thompson <audreydnthompson@gmail.com>
2017-02-21 00:48:07 -0800

    Made very basic game skeleton with background, player movement, and camera

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody2D rb;
+	float moveX;
+	float moveY;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Later, we will probably have to change the movement to be 8-directional (or 4-directional?)
+		// and to make the sprite turn/animate when it changes directions.
+		moveX = Input.GetAxis("Horizontal");
+		moveY = Input.GetAxis("Vertical");
+		Vector2 delta = new Vector2(moveX * speed, moveY * speed);
+		if (delta.magnitude > speed) {
+			delta.Normalize ();
+			delta = delta * speed;
+		}
+		rb.velocity = delta;
+	}
+}
